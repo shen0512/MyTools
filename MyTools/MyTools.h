74b1e92b1a88a5e7b9340d38d2ae5308d8f1b1ae
shen0512 <shen0512@github.com>
2022-07-03 00:02:03 +0800

    Initial Commit

---
+++ MyTools/MyTools.h
@@ -0,0 +1,18 @@
+//
+//  MyTools.h
+//  MyTools
+//
+//  Created by Shen on 2022/7/3.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyTools.
+FOUNDATION_EXPORT double MyToolsVersionNumber;
+
+//! Project version string for MyTools.
+FOUNDATION_EXPORT const unsigned char MyToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyTools/PublicHeader.h>
+
+
