d18061ae4da0dd5cfbad966113353dea3c0c5509
shen0512 <shen0512@github.com>
2024-08-18 20:08:09 +0800

    tmp

---
+++ MyTools/MyDebug/MyDebugParam.h
@@ -0,0 +1,16 @@
+//
+//  MyDebugParam.h
+//  MyTools
+//
+//  Created by Shen on 2024/8/17.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyDebugParam : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MyTools/MyDebug/MyDebugParam.m
@@ -0,0 +1,20 @@
+//
+//  MyDebugParam.m
+//  MyTools
+//
+//  Created by Shen on 2024/8/17.
+//
+
+#import "MyDebugParam.h"
+
+@implementation MyDebugParam
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
